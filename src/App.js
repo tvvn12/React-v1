692ea84f033f7a3fbb809d0b396015c43aa05da3
tvvn12 <tvvn12@gmail.com>
2022-10-04 22:58:28 +0700

    video47

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+import "./App.scss";
+import Header from "./Header/Header";
 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world with me</p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+    <div className="app-container">
+      <Header />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Provider>
 );
 
